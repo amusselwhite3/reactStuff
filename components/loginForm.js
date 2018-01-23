c5e442d4d0080cae2e28685725799f1276822525
Austin Musselwhite <amusselwhite@sapient.com>
2018-01-23 14:29:22 -0500

    Add button to login Form page

--- components/loginForm.js
+++ components/loginForm.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text, TextInput, StyleSheet } from 'react-native';
+import { Alert, AppRegistry, Button, View, Text, TextInput, StyleSheet } from 'react-native';
 
 export default class LoginForm extends Component {
+
+  onPress() {
+    Alert.alert("BUTTON HIT");
+  }
   render() {
       return (
+        <View>
           <View style={styles.container}>
             <TextInput style = {styles.input}
                       placeholder="Email" />
             <TextInput style = {styles.input}
                         placeholder = "Password" />
 
+          </View> 
+          <View style={styles.buttonContainer}>
+              <Button
+                onPress={this.onPress}
+                title="Click me"
+                raised={true}
+                theme='dark'
+              />
           </View>
+        </View>
       );
   }
 }
@@ -20,6 +34,13 @@ const styles = StyleSheet.create({
     padding: 20,
     borderWidth: 4
   },
+
+  buttonContainer: {
+    margin: 20,
+    flexDirection: 'column' ,
+    backgroundColor: '#00C01E'
+  }, 
+
 
   input: {
     height: 40,
