502a01407b8244ebb96f5b095e2eb7280ab9b3ed
Austin Musselwhite <amusselwhite@sapient.com>
2018-01-25 09:39:14 -0500

    quick table view

--- pages/profileScreen.js
+++ pages/profileScreen.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { 
      Text,
-     AppRegistry
+     AppRegistry, 
+     FlatList, 
+     View,
+     StyleSheet
 } from 'react-native';
 
 
@@ -12,10 +15,49 @@ export default class ProfileScreen extends React.Component {
     render() {
       const { navigate } = this.props.navigation;
       return (
-       <Text> Hello this is a placeholder page </Text>
-        
+        <View style = {styles.container}>
+            <FlatList
+                data={[
+                    {key: 'Devin'},
+                    {key: 'Jackson'},
+                    {key: 'James'},
+                    {key: 'Joel'},
+                    {key: 'John'},
+                    {key: 'Jillian'},
+                    {key: 'Jimmy'},
+                    {key: 'Julie'}, 
+                    {key: 'evin'},
+                    {key: 'ackson'},
+                    {key: 'ames'},
+                    {key: 'oel'},
+                    {key: 'ohn'},
+                    {key: 'illian'},
+                    {key: 'immy'},
+                    {key: 'ulie'}, 
+                    {key: 'vin'},
+                    {key: 'ckson'},
+                    {key: 'mes'},
+                    {key: 'el'},
+                    {key: 'hn'},
+                    {key: 'llian'},
+                    {key: 'mmy'},
+                    {key: 'lie'},
+                  ]}
+                  renderItem = {({item}) => <Text style={styles.item}>{item.key}</Text>}
+            />
+        </View>       
       );
     }
   }
+
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          padding:20
+      },
+      item: {
+          padding: 10,
+      }
+  })
 
   AppRegistry.registerComponent('ProfileScreen', () => ProfileScreen);
